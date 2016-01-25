b6c267b99d38786282b6981f5c05043f596531ad
Jacobsd1 <theduhannj@gmail.com>
2016-01-24 23:22:38 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+opposite=null
+set = function(x){
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+y <<- x
+opposite <<- null
 
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+get = function()y
+setopposite = function(inverse) opposite <<- inverse
+getopposite = function() opposite
+list(set=set, get=get, setopposite=setopposite, getopposite=getopposite)
 }
+
+cacheSolve <- function(y, ...) {
+
+cacheSolve <<- CacheMatrix
